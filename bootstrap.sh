01336d254e243d6345ce6ad3b8534238a55185b8
isBatak <ivicabatinic123@gmail.com>
2015-06-01 15:22:59 +0200

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='root'
+PROJECTFOLDER='klio'
+
+# create project folder
+sudo mkdir "/var/www/public_html/${PROJECTFOLDER}"
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install apache 2.5 and php 5.5
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5-mysql
+
+# install phpmyadmin and give password(s) to installer
+# for simplicity I'm using the same password for mysql and phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/public_html/${PROJECTFOLDER}"
+    ServerName ${PROJECTFOLDER}.dev
+	ServerAlias www.${PROJECTFOLDER}.dev
+	ErrorLog "/var/www/public_html/${PROJECTFOLDER}\error.log"
+	CustomLog "/var/www/public_html/${PROJECTFOLDER}\access.log" combined
+    <Directory "/var/www/public_html/${PROJECTFOLDER}">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+sudo apt-get -y install git
+
+# install Composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
